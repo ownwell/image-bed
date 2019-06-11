1d671e1b477d9451c98719784dcbb3ff949f52f8
cyning <ownwell@126.com>
2019-06-11 23:03:00 +0800

    blog_img

--- script/img.py
+++ script/img.py
@@ -8,7 +8,7 @@ print(os.path.abspath(__file__))
 
  
 imgUrl = ""
-path = "/Users/lixingyun/work/private/image/"
+path = "/Users/lixingyun/work/private/image/img/"
 def timestamp_to_date(time_stamp, format_string="%Y-%m-%d-%H-%M-%S"):
     time_array = time.localtime(time_stamp)
     str_date = time.strftime(format_string, time_array)
@@ -26,7 +26,7 @@ def saveClipboardImg():
         date = int(time.time())
         now = timestamp_to_date(date)
         filename =  str(now) + ".jpg"
-        imgUrl = "https://raw.githubusercontent.com/ownwell/image-bed/master/"+filename
+        imgUrl = "https://raw.githubusercontent.com/ownwell/image-bed/master/img/"+filename
         print(imgUrl)
         im.save(path +  filename, im.format)
         width, height = im.size
